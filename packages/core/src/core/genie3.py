sort columns before computation for correct and efficient indexing

packages/core/src/core/genie3.py
@@ -7,15 +7,15 @@ from tqdm.auto import tqdm
 
 from .config import RegressorConfig
 from .data import GRNDataset
-
-from .regressor import RegressorRegistry, CUDA_AVAILABLE
-
+from .regressor import CUDA_AVAILABLE, RegressorRegistry
 
 if CUDA_AVAILABLE:
     try:
         from cudf.pandas import install
+
         install()
         import cupy as cp
+
         xp = cp
     except ImportError:
         pass
@@ -43,23 +43,17 @@ def calculate_importances(
 ) -> ArrayLike:
     gene_expressions: ArrayLike = xp.asarray(dataset.gene_expressions.values)
 
-    transcription_factor_indices: List = dataset._transcription_factor_indices
-    transcription_factor_indices: ArrayLike = xp.asarray(
-        transcription_factor_indices, dtype=xp.uint32
-    )
-
     # Get the number of genes and transcription factors
     num_genes = gene_expressions.shape[1]
-    num_transcription_factors = len(transcription_factor_indices)
+    num_transcription_factors = len(dataset._transcription_factor_indices)
+
+    transcription_factor_indices: ArrayLike = xp.asarray(
+        dataset._transcription_factor_indices
+    )
 
     # Initialize the importance matrix
     importance_matrix = xp.zeros(
         (num_genes, num_transcription_factors), dtype=xp.float32
-    )
-
-    # Standardize data
-    gene_expressions = (gene_expressions - gene_expressions.mean(axis=0)) / (
-        gene_expressions.std() + 1e-8
     )
 
     gene_expressions, transcription_factor_indices, importance_matrix = (
@@ -85,8 +79,6 @@ def calculate_importances(
         )
         regressor.fit(X, y, regressor_config.fit_params)
         importance_matrix[target_gene, input_genes] = regressor.feature_importances_
-
-    importance_matrix = xp.asarray(importance_matrix)
     return importance_matrix
 
 
@@ -107,11 +99,11 @@ def rank_genes_by_importance(
         where transcription_factor and target_gene are gene names
     """
     rows = []
-    transcription_factor_indices: List = dataset._transcription_factor_indices
     gene_names: List = dataset._gene_names
-
     num_genes, num_transcription_factors = importance_matrix.shape
-
+    transcription_factor_indices: ArrayLike = xp.asarray(
+        dataset._transcription_factor_indices
+    )
     for i in range(num_genes):
         for j in range(num_transcription_factors):
             tf_idx = transcription_factor_indices[j]